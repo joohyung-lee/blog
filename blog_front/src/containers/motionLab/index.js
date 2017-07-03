2d4762d2301c74f64bbafe42b43154a7def56a1b
joomation <joomation@gmail.com>
2017-07-03 15:46:20 +0900

    리덕스를 통해 데이터 뿌려주기

--- blog_front/src/containers/motionLab/index.js
+++ blog_front/src/containers/motionLab/index.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
 import propTypes from 'prop-types';
-import axios from 'axios';
 import * as PostActions from 'redux/motionLab';
+
+const PostWrap=({title,index})=>{
+    return (
+        <li>{title}, {index}</li>
+    );
+}
 class MotionLab extends Component {
     componentDidMount() {
-        axios.get(`/api/motionlab`).then((response)=>{
-            console.log(response);
-        });
-        //PostActions.getPost();
+        const {postReducer}=this.props;
+        postReducer.getPost();
     }
     render() {
+        const{error,loading,post,number}=this.props;
         return (
             <div>
+                {loading && <h2>로딩중..</h2>}
+                {error?
+                    <h1>에러발생</h1>
+                    :(
+                        <ul>
+                            {
+                                this.props.post.map((postItem,number)=>{
+                                    return (<PostWrap key={number} index={number} title={postItem.title}/>)                              
+                                })
+                            }
+                           
+                        </ul>
+                    )}
                 <h1>MotionLab</h1>
             </div>
         );
@@ -22,4 +42,13 @@ MotionLab.propTypes = {
 
 };
 
-export default MotionLab;
+export default connect(
+    (state)=>({
+        loading:state.motionLab.pending,
+        post:state.motionLab.data,
+        error:state.motionLab.error,
+    }),
+    (dispatch)=>({ 
+        postReducer:bindActionCreators(PostActions,dispatch)
+    })
+)(MotionLab);
--- blog_front/src/redux/motionLab/index.js
+++ blog_front/src/redux/motionLab/index.js
@@ -18,7 +18,6 @@ export const postFailure = createAction(GET_POST_FAILURE);
 export const getPost = (postId) => dispatch => {
     // 먼저, 요청이 시작했다는것을 알립니다
     dispatch({type: GET_POST_PENDING});
-    console.log('start');
     // 요청을 시작합니다
     // 여기서 만든 promise 를 return 해줘야, 나중에 컴포넌트에서 호출 할 때 getPost().then(...) 을 할 수 있습니다
     return getPostAPI(postId).then(
@@ -30,14 +29,7 @@ export const getPost = (postId) => dispatch => {
                 payload: response
             })
         }
-    ).catch(error => {
-        console.log(error);
-        // 에러가 발생했을 경우, 에로 내용을 payload 로 설정하여 GET_POST_FAILURE 액션을 디스패치합니다.
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: error
-        });
-    })
+    )
 
 }
 
@@ -47,10 +39,12 @@ export const getPost = (postId) => dispatch => {
 const initialState = {
     pending: false,
     error: false,
-    data: {
-        title: '',
-        body: ''
-    }
+    data: [
+        {
+            title: '',
+            body: ''
+        }
+    ]
 }
 
 //리듀서
@@ -63,14 +57,10 @@ export default handleActions({
         };
     },
     [GET_POST_SUCCESS]: (state, action) => {
-        const { title, body } = action.payload.data;
-
         return {
             ...state,
             pending: false,
-            data: {
-                title, body
-            }
+            data:action.payload.data
         };
     },
     [GET_POST_FAILURE]: (state, action) => {
