af698b7e5b279e126d9ffa83cbc8dd1bcc8f8ddf
joomation <joomation@gmail.com>
2017-07-04 18:04:16 +0900

    세팅

--- blog_backend/app.js
+++ blog_backend/app.js
@@ -1,9 +1,36 @@
+//load plugin
 require('dotenv').config();
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
+var passport = require('passport'),
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+//passport session
+passport.serializeUser(function(user, done) {
+  // done(null, user.id);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  // Users.findById(obj, done);
+  done(null, obj);
+});
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+//set express
+var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json()); 
 
