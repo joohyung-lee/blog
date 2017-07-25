2ff841d57acc2d17e3d87ee983922020e3980411
joomation <joomation@github.com>
2017-07-25 21:05:33 +0900

    react-motion적용

--- blog_front/src/containers/main/index.js
+++ blog_front/src/containers/main/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import './test.scss';
+
+
 
 class Main extends Component {
+    constructor(props) {
+    super(props)
+    this.state = {
+        items:[
+            {key:'a',title:'title'},
+            {key:'b',title:'title'},
+            {key:'c',title:'title'},
+            {key:'d',title:'title'},
+            {key:'e',title:'title'},
+            {key:'f',title:'title'},
+            {key:'g',title:'title'},
+            {key:'h',title:'title'},
+            {key:'i',title:'title'},
+            {key:'j',title:'title'}
+        ]
+    }
+  }
+
     render() {
         return (
             <div>
-                <h1>Main</h1>
+            
             </div>
-        );
+
+        )
     }
 }
-
-Main.propTypes = {
-
-};
 
 export default Main;
