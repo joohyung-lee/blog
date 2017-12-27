aecf5159fcb1a88e7262271607994097f344b468
joomation <joomation@gmail.com>
2017-12-27 17:56:02 +0900

    remove tags index

--- blog_backend/src/model/post.js
+++ blog_backend/src/model/post.js
@@ -32,5 +32,5 @@ var postSchema = new Schema({
    
 },{ minimize: false });
 postSchema.plugin(mongoosePaginate);
-postSchema.index({title: 'text', summary: 'text',tags:1});
+postSchema.index({title: 'text', summary: 'text'});
 module.exports = mongoose.model('post',postSchema);
