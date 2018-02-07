14438dee815bdc029e986873c7fd33f4a48807aa
joomation <joomation@gmail.com>
2018-02-07 10:36:52 +0900

    apply preload

--- blog_front/src/components/main/cardItem.js
+++ blog_front/src/components/main/cardItem.js
@@ -150,7 +150,7 @@ class CardItem extends Component {
                                 thumbLoading?<DefaultLoading color="white"/>:null
                             }
                             {(this.props.isGif && videoSrc!=='')?
-                                <video className="video-wrap" loop playsInline src={videoSrc} ref={(ref)=>{this.videoSource=ref}}  
+                                <video className="video-wrap" preload loop playsInline src={videoSrc} ref={(ref)=>{this.videoSource=ref}}  
                                 style={{
                                     visibility:gifLoad?'visible':'hidden'
                                 }}>
