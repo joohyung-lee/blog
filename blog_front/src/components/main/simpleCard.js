a072cb02c5c16a216bcaa7a4d37e85a17cccc756
joomation <joomation@gmail.com>
2018-02-08 15:30:58 +0900

    facebook,github login

--- blog_front/src/components/main/simpleCard.js
+++ blog_front/src/components/main/simpleCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DefaultLoading from 'images/defaultLoading';
 import defaultAvatar from 'images/defaultAvatar.svg';
 import urlConfig from 'config/urlConfig';
+import Dotdotdot from 'react-dotdotdot';
 class SimpleCard extends Component {
     render() {
         return (
@@ -19,7 +20,9 @@ class SimpleCard extends Component {
                         <div className="post-meta">
                             <span className="category">{this.props.data.category}</span>
                         </div>
-                        <h3>{this.props.data.title}</h3>
+                        <Dotdotdot clamp={this.props.bottomHeight<140?1:2} className="post-title">
+                            <h3>{this.props.data.title}</h3>
+                        </Dotdotdot>
                         <div className="avatar-wrap">                       
                             <div className="avatar" 
                             style={{backgroundImage:`url(${this.props.currentUser.profileImg.isDefault?defaultAvatar:this.props.currentUser.profileImg.url})`}}
